775a5f63fd2e669822cd26213dd296b72187e41c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-03-22 08:31:20 -0500

    initial commit

---
+++ app/App.svelte
@@ -0,0 +1,15 @@
+<page>
+  <actionBar title="Svelte Native webView" />
+  <stackLayout class="p-20">
+    <webView src="<div>This works.</div>" />
+    <webView src="~/demo.html" />
+  </stackLayout>
+</page>
+
+<style>
+  webView {
+    border-width: 2;
+    height: 100;
+    margin-top: 10;
+  }
+</style>
